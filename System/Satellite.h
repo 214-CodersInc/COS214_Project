8fe712ede30edb391e98bdf8ac2e4c36db16d060
Thangeni FT <93540728+thangeniFT@users.noreply.github.com>
2021-11-22 15:15:40 +0200

    Add files via upload

---
+++ System/Satellite.h
@@ -0,0 +1,43 @@
+#ifndef Satellite_H
+#define Satellite_H
+#include "User.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Satellite
+{
+private:
+    /* data */
+    string name;
+    string status;
+
+    User* user;
+    double distance;
+    double distanceToOrbit;
+    double area;
+
+public:
+    Satellite(User*,string, string,double, double);
+    Satellite(const Satellite& sat);
+    Satellite* clone();
+
+    void sendSignal(); //This is void update()
+    void registerSatellite(User*);
+    void statusChange();
+    void setDistanceToOrbit(double dis); //new
+    void Communicate(); //communication between satellites
+
+    //string sendSignal(); //new -communicstion between user and satellite
+     //new
+    bool testDistance();
+
+    void setStatus(string s){status= s;}
+    string getName(){ return name;}
+    string getUserName(){ return user->getName();}
+    
+    ~Satellite();
+};
+
+
+#endif
