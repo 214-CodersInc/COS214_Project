401925b99a8f27500fdc9db6393acab54f92cf75
NYZCoder <94626748+NYZCoder@users.noreply.github.com>
2021-11-22 20:21:43 +0200

    Update Load.cpp

--- System/Load.cpp
+++ System/Load.cpp
@@ -5,23 +5,23 @@
 
 using namespace std;
 
-Load::Load(int loadCapacity)
+Load::Load(int loadCapacity)	//initialises Load Constuctor with loadCapacity variable and received variable
 {
 	this->loadCapacity = loadCapacity;
 	this->received = false;
 }
 
-void Load::setReceipt(bool b)
+void Load::setReceipt(bool b)	//sets the receipt variable 'received'
 {
 	this->received = b;
 }
 
-void Load::setCapacity(int c)
+void Load::setCapacity(int c)	//sets the capacity variable 'loadCapacity
 {
 	this->loadCapacity = c;
 }
 
-Load::~Load()
+Load::~Load()	//Load destructor
 {
 	
 }
