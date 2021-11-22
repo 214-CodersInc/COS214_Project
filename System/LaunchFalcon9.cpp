a7e83b6174edaad28920c6c6616b086012374080
Ponalo-ss <94800625+Ponalo-ss@users.noreply.github.com>
2021-11-23 01:31:04 +0200

    Update LaunchFalcon9.cpp

--- System/LaunchFalcon9.cpp
+++ System/LaunchFalcon9.cpp
@@ -14,36 +14,36 @@ void LaunchFalcon9::HandleRequest() /// Handling the requests of the engines bef
         merlinEngines[i]=merlinEngines[0]->clone();
     }
 
-    //Vacuum Engines
-    /* 
+    ///Vacuum Engines
+    /** 
     240<=Temp<350
     50 <=oilPresure<=100
     40<=FlowGradient<=50
     */
     Engine* vacuumEngine=new VacuumEngine(280,55,40,100);
 
-    //Falcon 9 Rocket
-    //adding engines
+    ///Falcon 9 Rocket
+    ///adding engines
     for(int i=0;i<9;i++)
     {
         rocket->addMerlinEngine(merlinEngines[i]);
     }
-    //adding vacuum engine
+    ///adding vacuum engine
     rocket->addVacuumEngine(vacuumEngine);
 
 
 
 
-    //Simulate the rockets
+    ///Simulate the rockets
     LaunchSimulator* DroneShip=new LaunchSimulator();
     DroneShip->addRocket(rocket);
 
     DroneShip->launchRocket(0);  //test rocket 1 
 
     cout<<"Batch Testing the Rockets"<<endl;
-    //DroneShip->batchLaunch();
+    ///DroneShip->batchLaunch();
 
-    //DELETING
+    ///DELETING
     for(int i=0;i<9;i++)
     {
         delete merlinEngines[i];
