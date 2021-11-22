3507aac9d0147a10af648cfaf8242eebe9b634d3
Ponalo-ss <94800625+Ponalo-ss@users.noreply.github.com>
2021-11-23 00:25:02 +0200

    Update Falcon9.cpp

--- System/Falcon9.cpp
+++ System/Falcon9.cpp
@@ -1,23 +1,23 @@
 #include "Falcon9.h"
 
-Falcon9::Falcon9()
+Falcon9::Falcon9()              ///Constructor
 {
 }
 
 
 
-bool Falcon9::stage1Test()
+bool Falcon9::stage1Test()       ///Stage1 test for falcon9 engine
 {
     cout<<"Initiating stage1 test of a Falcon9"<<endl;
     return Rocket::stage1Test();
 }
 
-bool Falcon9::stage2Test()
+bool Falcon9::stage2Test()    ///Stage2 test for falcon9 engine
 {
     cout<<"Initiating Stage 2 test of Falcon 9"<<endl;
     return Rocket::stage2Test();
 }
 
-Falcon9::~Falcon9()
+Falcon9::~Falcon9()    ///Destructor
 {
 }
