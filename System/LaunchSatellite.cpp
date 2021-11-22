0de969d4bc45af3645502249d20708dc5e3278b0
Coders_Inc <faresatheo1@gmail.com>
2021-11-22 20:44:19 +0200

    paste

--- System/LaunchSatellite.cpp
+++ System/LaunchSatellite.cpp
@@ -20,6 +20,39 @@ void LaunchSatellite::HandleRequest()
 
    
  
+    User* Nally=new User("Nally con");
+    User* Nate=new User("Nate");
+
+    string setalliteName= "Satellite A";
+    string stat="on Earth";
+    double dist=0; //in km
+    double distanceToOrbit= 24000;
+    double area= 36000;
+
+    Satellite* setallite=new Satellite(Nally,setalliteName, stat, dist ,area);
+
+    setallite->updateDistance();
+    setallite->testDistance();
+
+    setallite->statusChange();
+
+    for(int i=0; i<120; i++)
+    {
+        if(setallite->getDistance() >=distanceToOrbit)
+        {
+            setallite->updateDistance();
+            break;
+        }
+    }
+    setallite->statusChange();
+
+    
+
+
+
+    cout<<"Beginning Satellite simulation....."<<endl;
+    cout<<" "<<endl;
+
    
     cout<<" "<<endl;
     cout<<"==========End of simulation=========="<<endl;
