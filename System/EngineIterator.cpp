599ab2a44a9af0b751d0f3d5ec3970cb0350eb57
Ponalo-ss <94800625+Ponalo-ss@users.noreply.github.com>
2021-11-23 00:55:20 +0200

    Update EngineIterator.cpp

--- System/EngineIterator.cpp
+++ System/EngineIterator.cpp
@@ -1,11 +1,11 @@
 #include "EngineIterator.h"
 
-EngineIterator::EngineIterator(vector<Engine*> lis)
+EngineIterator::EngineIterator(vector<Engine*> lis)  //
 {
     list=lis;
     currentPos=0;
 }
 
-EngineIterator::~EngineIterator()
+EngineIterator::~EngineIterator()  // Destructor
 {
 }
