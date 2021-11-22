1e04ca449d9678e010b652f5cde2b9c71ffbea1d
Ponalo-ss <94800625+Ponalo-ss@users.noreply.github.com>
2021-11-23 00:20:43 +0200

    Update Engine.cpp

--- System/Engine.cpp
+++ System/Engine.cpp
@@ -1,36 +1,36 @@
 #include "Engine.h"
 
-Engine::Engine(int num1,int num2,int num3,int num4):temp(num1),pressure(num2),fGradient(num3),fuel(num4)
+Engine::Engine(int num1,int num2,int num3,int num4):temp(num1),pressure(num2),fGradient(num3),fuel(num4)   ///Constructor
 {
 }
 
-/*void Engine::refill()
+/*void Engine::refill()  ///Refuel the engine
 {
     fuel=100;
 }*/
 
-void Engine::On()
+void Engine::On()    ///Switch engine on
 {
     on=true;
 }
 
-void Engine::Off()
+void Engine::Off()   ///Swtch engine off
 {
     on=false;
 }
 
-EngineMemento *Engine::createMemento()
+EngineMemento *Engine::createMemento()      ///A memento  to restore the state of the Engine
 {
     return new EngineMemento(fuel,temp);
 }
 
-void Engine::retrieveMemento(EngineMemento* engi)
+void Engine::retrieveMemento(EngineMemento* engi)    ///Restoring the state of fuel and temperature
 {
     this->fuel=engi->getFuel();
     this->temp=engi->getTemp();
 }
 
-Engine::~Engine()
+Engine::~Engine()   ///Destructor
 {
     
 }
