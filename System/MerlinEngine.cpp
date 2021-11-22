f6583aa20babf641b859600605cf0fc535543497
Ponalo-ss <94800625+Ponalo-ss@users.noreply.github.com>
2021-11-23 01:26:20 +0200

    Update MerlinEngine.cpp

--- System/MerlinEngine.cpp
+++ System/MerlinEngine.cpp
@@ -13,19 +13,19 @@ bool MerlinEngine::testEngine()   ///Test engine function to test all the aspect
     if(on)
     {
         EngineStore* storage=new EngineStore();
-        storage->storeMemento(this->createMemento());    //store the Initial Temp and fuel of engine before testing
+        storage->storeMemento(this->createMemento());    ///store the Initial Temp and fuel of engine before testing
         fuel-=25; 
         temp+=4;
         if(fGradient>=30 && fGradient<40)  //Flow gradi3ent between 30 and 40
         {
             status=true;
-            if(pressure>=30 && pressure<50 && status)  //oil pressure  between 30 and 50 
+            if(pressure>=30 && pressure<50 && status)  ///oil pressure  between 30 and 50 
             {
                 fuel-=25;
                 temp+=4;
-                if(temp>=200 && temp<250 && status)  //Temperature between 200 and 250
+                if(temp>=200 && temp<250 && status)  ///Temperature between 200 and 250
                 {
-                    retrieveMemento(storage->retrieve());     //restore the original fuel and temperature values
+                    retrieveMemento(storage->retrieve());     ///restore the original fuel and temperature values
                 }
                 else
                 {
