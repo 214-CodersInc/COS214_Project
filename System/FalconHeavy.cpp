680a81dcd7f0f56f7eaf2d1519cb977dffea8870
Ponalo-ss <94800625+Ponalo-ss@users.noreply.github.com>
2021-11-23 00:27:49 +0200

    Update FalconHeavy.cpp

--- System/FalconHeavy.cpp
+++ System/FalconHeavy.cpp
@@ -1,23 +1,23 @@
 #include "Falconheavy.h"
 
-FalconHeavy::FalconHeavy()
+FalconHeavy::FalconHeavy()     ///Constructor
 {
 
 }
 
 
-bool FalconHeavy::stage1Test()  //
+bool FalconHeavy::stage1Test()  ///Stage1 test for Falcon heavy engine
 {
     cout<<"Initiating stage1 test of a FalconHeavy"<<endl;
     return Rocket::stage1Test();
 }
 
-bool FalconHeavy::stage2Test()
+bool FalconHeavy::stage2Test()   ///Stage2 test for Falcon heavy engine
 {
     cout<<"Initiating Stage 2 test of FalconHeavy"<<endl;
     return Rocket::stage2Test();
 }
 
-FalconHeavy::~FalconHeavy()
+FalconHeavy::~FalconHeavy()   ///Destructor
 {
 }
