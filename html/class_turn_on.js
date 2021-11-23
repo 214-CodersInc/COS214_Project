1f8129568c1180103d1e9bbef149066d43a1585d
misbah-osman <75487122+misbah-osman@users.noreply.github.com>
2021-11-23 03:52:04 +0200

    Add files via upload

---
+++ html/class_turn_on.js
@@ -0,0 +1,7 @@
+var class_turn_on =
+[
+    [ "TurnOn", "class_turn_on.html#a83fc58c8101c3427baf86672be0df53f", null ],
+    [ "~TurnOn", "class_turn_on.html#aba20e0f2d3233baff1bfe8aa0a7bc380", null ],
+    [ "execute", "class_turn_on.html#a02bbbdaedfed01544ec91e458ad9911f", null ],
+    [ "undo", "class_turn_on.html#ab77123a825feb3f295e9906ca2311352", null ]
+];
---
+++ html/class_user.js
@@ -0,0 +1,11 @@
+var class_user =
+[
+    [ "User", "class_user.html#a4a0137053e591fbb79d9057dd7d2283d", null ],
+    [ "User", "class_user.html#ae8fcf5b3426fc4560ff092d0c825e3f7", null ],
+    [ "~User", "class_user.html#ac00b72ad64eb4149f7b21b9f5468c2b2", null ],
+    [ "attach", "class_user.html#ae22805a1114446479e48223ff2902c66", null ],
+    [ "detach", "class_user.html#aa3fc04f5af50c21f1ba42082b019a90a", null ],
+    [ "getName", "class_user.html#adb316ac38d5f62a967686e7b736a0469", null ],
+    [ "notify", "class_user.html#aac8479a377a4237832591290c70a0b87", null ],
+    [ "setName", "class_user.html#a5370952f73a8d10d7c95d0f9c782603b", null ]
+];
